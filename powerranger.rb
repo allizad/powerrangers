298f8672733a097fd8e163b42647fd26bae500ae
AlliZad <allison@zadrozny.com>
2014-06-18 12:39:01 -0500

    Create person class

--- powerranger.rb
+++ powerranger.rb
@@ -1,8 +1,56 @@
 class Person
+
+	attr_accessor :name
+	attr_reader :caffeine_level
+
+	def initialize(name, caffeine_level=200)
+		@name = name
+		@caffeine_level = caffeine_level
+	end
+
+	def caffeine_level
+		puts "#{@caffeine_level} mg."
+	end
+
+	def run(miles)
+
+		if @caffeine_level > 0
+			if miles < 10 && miles > 1
+				puts "#{@name.capitalize} has run #{miles} miles."
+				@caffeine_level -= (miles * 60)
+			elsif miles == 1
+				puts "#{@name.capitalize} has run #{miles} mile."
+				@caffeine_level -= (miles * 60)
+			elsif miles < 0
+				puts "#{@name} cannot run negative miles."
+			else
+				puts "#{@name.capitalize} has run #{miles} miles."
+				@caffeine_level -= (miles * 60)
+			end
+		else
+			puts "#{self.name} does not have enough caffeine to run."
+		end
+
+		if @caffeine_level < 0
+			puts "Caffeine withdrawal. Recaffeinate #{self.name} with #{((@caffeine_level.abs)/95).floor} cups of coffee."
+		end
+
+	end
+
+	def scream
+		puts "#{@name} says: 'BLEAAAAURAAGGGGHHHHH!!' "
+	end
+
+	def drink_coffee(cups)
+		@caffeine_level += (cups * 95)
+		puts "#{self.name} has a caffeine level of #{@caffeine_level} mg."
+		puts "#{self.name} is incapacitated for caffeine overdose." if @caffeine_level > 400
+	end
+
 end
 
-class PowerRanger
+class PowerRanger < Person
 end
 
-class EvilNinja
+class EvilNinja < Person
 end
---
+++ testcode.rb
@@ -0,0 +1,10 @@
+require_relative 'powerranger'
+
+clark_kent = Person.new("Clark")
+clark_kent.run(150)
+clark_kent.run(1)
+clark_kent.caffeine_level
+clark_kent.run(-2)
+clark_kent.caffeine_level
+clark_kent.scream
+clark_kent.drink_coffee(162)
